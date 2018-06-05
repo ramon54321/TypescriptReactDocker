ac247d6a6119cac157672d39f30698af2aad564c
Ramon Brand <ramon2009@gmail.com>
2018-06-05 10:29:29 +0300

    Ejs setup.

--- web/index.js
+++ web/index.js
@@ -1,9 +1,24 @@
 const express = require("express")
 const app = express()
 
+const redis = require("redis")
+const client = redis.createClient("redis://redis")
+
+const ejs = require("ejs")
+
+client.on("error", function (err) {
+    console.log("Error " + err)
+})
+
+const people = ['geddy', 'neil', 'alex']
+client.set("mykey", JSON.stringify({people: people}))
+
 app.get('/', (req, res) => {
-    console.log("Serving...")
-    res.send('Hello World Again 3!')
+    client.get("mykey", (err, result) => {
+        const html = ejs.render('<h2><%= people.join(" - "); %></h2>', JSON.parse(result))
+        console.log("Serving...")
+        res.send(html)
+    })
 })
 
 app.listen(80, () => console.log('Listening on port 80!'))
