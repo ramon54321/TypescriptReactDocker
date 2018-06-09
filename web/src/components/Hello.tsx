91214b5f67a3a95495de89a13126304a91bb48d8
Ramon Brand <ramon2009@gmail.com>
2018-06-09 18:17:01 +0300

    Small fix on Hello.tsx

--- web/src/components/Hello.tsx
+++ web/src/components/Hello.tsx
@@ -5,8 +5,6 @@ export interface HelloProps {
     framework: string
 }
 
-// 'HelloProps' describes the shape of props.
-// State is never set so we use the '{}' type.
 export class Hello extends React.Component<{}, {}> {
     log = () => {
         console.log("Clicked")
